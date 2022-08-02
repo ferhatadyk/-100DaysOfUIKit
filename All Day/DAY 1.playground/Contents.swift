bbc2dacfea5dbe5e3be1cfff6eb3cc267678e120
Ferhat Adiyeke <76595188+ferhatadyk@users.noreply.github.com>
2022-08-02 19:02:23 +0300

    day 1
    day 1

---
+++ All Day/DAY 1.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+
+
+// variables
+
+var str = "Hello world Ferhat"
+str = "ferhat"
+str = "adiyeke"
+
+// strings and integers
+var age = 38
+var population = 8_000_000
+var meaningOfLife = 42
+meaningOfLife = 42
+
+// multi-line strings
+
+var str1 = """
+thist goes
+over multiple
+lines
+"""
+
+var str2 = """
+Thist goes \
+over multiple \
+lines
+"""
+
+//doubles and booleans
+
+var pi = 3.141
+var awesome = true
+var awe = false
+
+// string interpolation
+
+var score = 85
+var st = "You score was \(score)"
+var results = "The test results are here: \(st)"
+
+var name = "Ferhat"
+var csd = "Merhaba Benim adım \(name)"
+var cd = "\(csd) 22 yaşındayım"
+var city = "Istanbul"
+var message = "Welcome to \(city)!"
+
+// constants
+
+let cs = "Sabit"
+
+// type annotations
+
+let stra = "Hello, playground"
+
+let album: String = "Repupation"
+let year: Int = 1976
+let height: Double = 1.0
+let rjo: Bool = true
+var SurName: String
+SurName = "Fer"
