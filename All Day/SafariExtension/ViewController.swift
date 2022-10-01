a6cec0c05e2e9fde523e1ca3bb5016ba91ebb59c
Ferhat Adiyeke <76595188+ferhatadyk@users.noreply.github.com>
2022-10-01 20:32:41 +0300

    Safari Extension
    Safari Extension

---
+++ All Day/SafariExtension/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafariExtension
+//
+//  Created by Ferhat Adiyeke on 1.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
