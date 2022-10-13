695f091b5a5237a1c6dda9a6c4fa9b53c97ff25f
Ferhat Adiyeke <76595188+ferhatadyk@users.noreply.github.com>
2022-10-14 02:02:31 +0300

    WhiteHousePetitions
    WhiteHousePetitions

---
+++ All Day/WhiteHousePetitions/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  WhiteHousePetitions
+//
+//  Created by Ferhat Adiyeke on 4.09.2022.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+    
+}
---
+++ All Day/WhiteHousePetitions/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  WhiteHousePetitions
+//
+//  Created by Ferhat Adiyeke on 4.09.2022.
+//
+
+import Foundation
+
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
